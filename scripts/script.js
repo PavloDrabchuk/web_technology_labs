49afa15903663b99b8a5d6807c4e85828d18e16e
PavloDrabchuk <ravluk2000@gmail.com>
2020-11-01 16:37:52 +0200

    lab2

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,7 @@ function calculateHypotenuse() {
 
     if (leg1 > 0 && leg2 > 0) {
         let hypotenuse = Number(Math.sqrt(Math.pow(leg1, 2) + Math.pow(leg2, 2)).toFixed(4));
-        //alert(typeof(hypotenuse).toString());
-        (isFinite(hypotenuse)) ? document.getElementById("result").innerText = "Гіпотенуза: " + hypotenuse: alert("Ви ввели занадто велике число.");
+        (isFinite(hypotenuse)) ? document.getElementById("result").innerText = "Гіпотенуза: " + hypotenuse : alert("Ви ввели занадто велике число.");
     } else {
         alert("Ви помилились під час введення значення.\nПеревірте їх та повторіть спробу.");
     }
